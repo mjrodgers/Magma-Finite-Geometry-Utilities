39cbc2d72b15d65799f157c516cdf5409d9f351a
Morgan Rodgers <morgan.joaquin@gmail.com>
2022-10-28 17:12:37 +0300

    allow iteration over subsets of a specified size

--- FiniteGeometry/SubsetProcess.m
+++ FiniteGeometry/SubsetProcess.m
@@ -91,3 +91,66 @@ intrinsic SubsetProcess(S::SetEnum) -> Process
 {Gives a process for iterating through all subsets of S.}
   return SubsetProcess(SetToIndexedSet(S));
 end intrinsic;
+
+
+intrinsic InternalkSubsetProcessIsEmpty(p::Tup) -> BoolElt
+{Returns true iff the transitive group process has passed its last group}
+    return p[4];
+end intrinsic;
+
+intrinsic InternalNextkSubset(~p::Tup)
+{Moves the subset process tuple p to its next subset}
+  error if InternalkSubsetProcessIsEmpty(p), "Process finished";
+  if p[3] le 0 then
+    p[4] := true;
+  else
+    p[2] := TransversalProcessNext(p[1]);
+    p[3] := TransversalProcessRemaining(p[1]);
+  end if;
+end intrinsic;
+
+intrinsic InternalExtractkSubset(p::Tup) -> { }
+{Returns the current subset of the transitive group process tuple p}
+    error if InternalkSubsetProcessIsEmpty(p), "Process finished";
+    return (p[5])(p[2]);
+end intrinsic;
+
+intrinsic InternalExtractkSubsetLabel(p::Tup) -> RngIntElt, SetEnum
+{Returns the index of the current subset, along with the parent set.}
+    error if InternalkSubsetProcessIsEmpty(p), "Process finished";
+    return p[2];
+end intrinsic;
+
+// TODO: should create simple workarounds when k=0 or k=n.
+// This can probably be modified to be much more efficient by avoiding the use of TransversalProcess
+intrinsic SubsetProcess(n::RngIntElt, k::RngIntElt) -> Process
+{Gives a process for iterating through all subsets from a set of size n having size k.}
+  requirerange k, 0, n;
+  P := TransversalProcess(Sym(n), DirectProduct(Sym(k),Sym(n-k)));
+  f := func<sigma | {1..k}^(sigma^-1)>;
+  info := <P, TransversalProcessNext(P), TransversalProcessRemaining(P), false, f>;
+  P := InternalCreateProcess(
+        "kSubsets",
+        info,
+        InternalkSubsetProcessIsEmpty,
+        InternalNextkSubset,
+        InternalExtractkSubset,
+        InternalExtractkSubsetLabel
+      );
+
+  return P;
+end intrinsic;
+
+intrinsic SubsetProcess(S::SetIndx, k::RngIntElt) -> Process
+{Gives a process for iterating through all subsets of S.}
+  requirerange k, 0, #S;
+  P := SubsetProcess(#S,k);
+  f := func<s | {S[i] : i in s} >;
+  return ModifyProcess(P, f);
+end intrinsic;
+
+intrinsic SubsetProcess(S::SetEnum, k::RngIntElt) -> Process
+{Gives a process for iterating through all subsets of S.}
+  requirerange k, 0, #S;
+  return SubsetProcess(SetToIndexedSet(S), k);
+end intrinsic;
