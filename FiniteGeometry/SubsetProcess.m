ba0be44845d3b4fd4c170c5490c3a2e9a53d169e
Morgan Rodgers <morgan.joaquin@gmail.com>
2022-10-28 16:53:56 +0300

    Clean up some details of simple SubsetProcess instrinsic

--- FiniteGeometry/SubsetProcess.m
+++ FiniteGeometry/SubsetProcess.m
@@ -2,14 +2,28 @@ freeze;
 
 /* -------------------------------------------------------------------------//
 // -------------------------------------------------------------------------//
-  Allows creation of a Process P := SubsetProcess(Set::S)
-  which allows iteration through the subsets of S,
-  beginning with the empty set.
-  We also have the standard "Process" commands:
+  Allows the creation of several Processes to iterate through
+  collections of subsets and subspaces.
+  These all allow the standard "Process" commands:
     IsEmpty(P)
     Current(P)
     CurrentLabel(P)
     Advance(P)
+  The following Process constructors are defined:
+    P := SubsetProcess(n::RngIntElt)
+      Allows iterating through all subsets of {1..n}, beginning with the empty set.
+    P := SubsetProcess(S::SetIndx)
+    P := SubsetProcess(S::SetEnum)
+      allows iteration through the subsets of S, beginning with the empty set.
+    TODO: Multiset version?
+    P := SubsetProcess(n::RngIntElt, k::RngIntElt)
+    P := SubsetProcess(S::SetIndx, k::RngIntElt)
+    P := SubsetProcess(S::SetEnum, k::RngIntElt)
+      As above, but only iterates through subsets of size k.
+    P := SubspaceProcess(U::ModTupFld, k::RngIntElt)
+      allows iteration through all subspaces of a finite vector space U
+      having dimension k.
+    TODO: For completion, should have a generic version that goes through ALL subspaces.
 // -------------------------------------------------------------------------//
 // -------------------------------------------------------------------------*/
 
@@ -29,58 +43,51 @@ end intrinsic;
 
 intrinsic InternalSubsetProcessIsEmpty(p::Tup) -> BoolElt
 {Returns true iff the transitive group process has passed its last group}
-    return (p[3] eq p[2]);
+    return (p[2] ge p[1]);
 end intrinsic;
 
 intrinsic InternalNextSubset(~p::Tup)
 {Moves the subset process tuple p to its next subset}
   error if InternalSubsetProcessIsEmpty(p), "Process finished";
-
-  p[3] +:= 1;
+  p[2] +:= 1;
 end intrinsic;
 
 intrinsic InternalExtractSubset(p::Tup) -> { }
 {Returns the current subset of the transitive group process tuple p}
     error if InternalSubsetProcessIsEmpty(p), "Process finished";
-
-    return {p[1][i] : i in IntToSet(p[3]) };
+    return { i : i in IntToSet(p[2]) };
 end intrinsic;
 
 intrinsic InternalExtractSubsetLabel(p::Tup) -> RngIntElt, SetEnum
 {Returns the index of the current subset, along with the parent set.}
     error if InternalSubsetProcessIsEmpty(p), "Process finished";
-
-    return p[3], p[1];
+    return p[2];
 end intrinsic;
 
-intrinsic SubsetProcess(S::SetEnum) -> Process
-{Gives a process for iterating through all subsets of S.}
-  tup := <SetToIndexedSet(S), 2^#S, 0 >;
 
-  P := InternalCreateProcess(
-        "Subsets",
-        tup,
-        InternalSubsetProcessIsEmpty,
-        InternalNextSubset,
-        InternalExtractSubset,
-        InternalExtractSubsetLabel
-      );
+intrinsic SubsetProcess(n::RngIntElt) -> Process
+{Gives a process for iterating through all subsets from a set of size n.}
+tup := <2^n, 0>;
+P := InternalCreateProcess(
+      "Subsets",
+      tup,
+      InternalSubsetProcessIsEmpty,
+      InternalNextSubset,
+      InternalExtractSubset,
+      InternalExtractSubsetLabel
+    );
 
-  return P;
+return P;
 end intrinsic;
 
 intrinsic SubsetProcess(S::SetIndx) -> Process
 {Gives a process for iterating through all subsets of S.}
-  tup := < S, 2^#S, 0 >;
+  P := SubsetProcess(#S);
+  f := func<s | {S[i] : i in s} >;
+  return ModifyProcess(P, f);
+end intrinsic;
 
-  P := InternalCreateProcess(
-        "Subsets",
-        tup,
-        InternalSubsetProcessIsEmpty,
-        InternalNextSubset,
-        InternalExtractSubset,
-        InternalExtractSubsetLabel
-      );
-
-  return P;
+intrinsic SubsetProcess(S::SetEnum) -> Process
+{Gives a process for iterating through all subsets of S.}
+  return SubsetProcess(SetToIndexedSet(S));
 end intrinsic;
